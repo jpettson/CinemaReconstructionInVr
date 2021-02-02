c3485b7246d4def638b376477ae1d10fc083e012
jpettson <johan.jp1@gmail.com>
2021-02-02 08:15:04 +0100

    Possibility to choose and play video is added

---
+++ Assets/Scripts/Video.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.Video;
+
+using System;
+
+public class Video : MonoBehaviour{
+
+public string path;
+private VideoPlayer videoPlayer;
+private VideoSource videoSource;
+private AudioSource audioSource;
+
+public void Start()
+{
+    Destroy(videoPlayer);
+    Application.runInBackground = true;
+    StartCoroutine(playVideo());
+}
+
+ public void Update()
+ {
+      if (Input.GetKeyDown(KeyCode.Space))
+     {
+         if (videoPlayer.isPlaying) {
+            videoPlayer.Pause();
+         } else if (videoPlayer.isPaused) {
+             videoPlayer.Play();
+         }
+     }
+
+     if (Input.GetKeyDown(KeyCode.RightArrow)) {
+         videoPlayer.frame += 100;
+         videoPlayer.Play();
+     } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+         videoPlayer.frame -= 100;
+         videoPlayer.Play();
+     }
+
+ }
+
+IEnumerator playVideo()
+{
+    videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+    videoPlayer.playOnAwake = true;
+
+    videoPlayer.source = VideoSource.Url;
+    videoPlayer.url = path;
+
+    videoPlayer.Prepare();
+
+    while (!videoPlayer.isPrepared)
+    {
+        Debug.Log("Preparing Video");
+        yield return null;
+    }
+
+    videoPlayer.Play();
+}
+
+}
