a0f694c6b6776308c52b9bdf4a453671244c9832
Adam Rohdell <adam.rohdell@gmail.com>
2021-02-17 17:32:44 +0100

    Auto stash before merge of "dev" and "origin/dev"
    Edited for task 3.1.3 as well

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+using UnityEngine.Video;
+public class VideoController : NetworkBehaviour
+{
+    [SerializeField]
+    private GameObject videoPlayerPrefab;
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    private string path;
+
+    private void Start()
+    {
+       CmdCreateVideo();        
+    }
+
+
+    void Update()
+    {
+        if (this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            this.RpcPause();
+        }
+    }
+
+    public void CmdCreateVideo()
+    {
+        videoPlayer = GameObject.FindGameObjectWithTag("VideoPlayer").GetComponent<VideoPlayer>();
+
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = path;
+
+        videoPlayer.Prepare();
+
+        videoPlayer.Play();
+    }
+
+    [ClientRpc]
+    void RpcPause()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+        } else
+        {
+            videoPlayer.Play();
+        }
+    }
+}
