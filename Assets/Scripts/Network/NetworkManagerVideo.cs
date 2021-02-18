5b6056fc11d9e6fa7d47ac37846ff77753f9eea1
Adam Rohdell <adam.rohdell@gmail.com>
2021-02-18 19:16:31 +0100

    Continued work on task 3.1.3, more work required

---
+++ Assets/Scripts/Network/NetworkManagerVideo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Video;
+
+public class NetworkManagerVideo : NetworkManager
+{
+    [SerializeField]
+    public Transform startLocation;
+    [SerializeField]
+    public GameObject videoScreenPrefab;
+    [SerializeField]
+    public GameObject videoScreenObject;
+
+    public static string videoName = "";
+    public static bool isVideoPlaying = false;
+    public static long currentFrame;
+
+    private int x = 0;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        GameObject player = Instantiate(playerPrefab, startLocation.position, startLocation.rotation);
+        GameObject videoPlayer = Instantiate(videoScreenPrefab, videoScreenObject.transform);
+        NetworkIdentity playerId = player.GetComponent<NetworkIdentity>();
+
+        
+        
+        NetworkServer.AddPlayerForConnection(conn, player);
+        playerId.AssignClientAuthority(conn);
+
+        NetworkServer.Spawn(videoPlayer, player);
+    }
+}
--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -6,47 +6,90 @@ using System;
 using UnityEngine.Video;
 public class VideoController : NetworkBehaviour
 {
+    public int x;
     [SerializeField]
-    private GameObject videoPlayerPrefab;
-    private VideoPlayer videoPlayer;
-    [SerializeField]
-    private string path;
+    public VideoPlayer videoPlayer;
 
     private void Start()
     {
-       CmdCreateVideo();        
-    }
-
-
-    void Update()
-    {
-        if (this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        foreach (KeyValuePair<uint, NetworkIdentity> entry in NetworkIdentity.spawned)
         {
-            this.RpcPause();
+           if (entry.Value.netId == GetComponentInParent<NetworkIdentity>().netId + 1)
+            {
+                videoPlayer = entry.Value.gameObject.GetComponentInChildren<VideoPlayer>();
+            }
+        }
+        if (NetworkManagerVideo.isVideoPlaying)
+        {
+            InitializeVideoPlayer();
+
+            // videoPlayer.url = Application.dataPath + "/" + NetworkManagerVideo.videoName;
+            videoPlayer.url = "C:\\Users\\adam_\\Downloads\\yt1s.com - Rick Astley  Never Gonna Give You Up Video_360p.mp4";
+            videoPlayer.Prepare();
+
+            videoPlayer.frame = NetworkManagerVideo.currentFrame;
+            videoPlayer.Play();
         }
     }
 
-    public void CmdCreateVideo()
+    public void InitializeVideoPlayer()
     {
-        videoPlayer = GameObject.FindGameObjectWithTag("VideoPlayer").GetComponent<VideoPlayer>();
-
-        videoPlayer.source = VideoSource.Url;
-        videoPlayer.url = path;
-
-        videoPlayer.Prepare();
-
-        videoPlayer.Play();
+        Debug.Log(x);
+        videoPlayer.targetMaterialRenderer = videoPlayer.GetComponentInParent<MeshRenderer>();
     }
 
+    void Update()
+    {
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("memes2");
+            CmdPause();
+            
+        } else if (isLocalPlayer && Input.GetKeyDown(KeyCode.G))
+        {
+            
+            CmdLoadVideo("C:\\Users\\adam_\\Downloads\\yt1s.com - Rick Astley  Never Gonna Give You Up Video_360p.mp4");
+        }
+        if (videoPlayer.isPlaying)
+        {
+            NetworkManagerVideo.currentFrame = videoPlayer.frame;
+        }
+    }
+   
+    [Command]
+    void CmdPause()
+    {
+        RpcPause();
+        NetworkManagerVideo.isVideoPlaying = !NetworkManagerVideo.isVideoPlaying;
+    }
     [ClientRpc]
     void RpcPause()
     {
         if (videoPlayer.isPlaying)
         {
             videoPlayer.Pause();
-        } else
+        }
+        else
         {
             videoPlayer.Play();
         }
     }
+
+
+    [Command]
+    public void CmdLoadVideo(string path)
+    {
+        RpcLoadVideo(path);
+    }
+
+    [ClientRpc]
+    void RpcLoadVideo(string path)
+    {
+        InitializeVideoPlayer();
+
+        videoPlayer.url = path;
+
+        videoPlayer.Prepare();
+    }
+
 }
