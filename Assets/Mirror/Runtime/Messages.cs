eada1f6c4cbf0a6dade4edf40e8adc92430c6411
Joakim <burmanj@student.chalmers.se>
2021-02-18 13:12:24 +0100

    Mirror fix

--- Assets/Mirror/Runtime/Messages.cs
+++ Assets/Mirror/Runtime/Messages.cs
@@ -14,25 +14,11 @@ namespace Mirror
     public interface NetworkMessage { }
 
     #region Public System Messages
-    public struct ErrorMessage : NetworkMessage
-    {
-        public byte value;
-
-        public ErrorMessage(byte v)
-        {
-            value = v;
-        }
-    }
-
     public struct ReadyMessage : NetworkMessage { }
 
     public struct NotReadyMessage : NetworkMessage { }
 
     public struct AddPlayerMessage : NetworkMessage { }
-
-    public struct DisconnectMessage : NetworkMessage { }
-
-    public struct ConnectMessage : NetworkMessage { }
 
     public struct SceneMessage : NetworkMessage
     {
@@ -51,7 +37,7 @@ namespace Mirror
 
     #endregion
 
-    #region System Messages requried for code gen path
+    #region System Messages required for code gen path
     public struct CommandMessage : NetworkMessage
     {
         public uint netId;
--- Assets/Mirror/Runtime/NetworkBehaviour.cs
+++ Assets/Mirror/Runtime/NetworkBehaviour.cs
@@ -494,7 +494,7 @@ namespace Mirror
         public struct NetworkBehaviourSyncVar : IEquatable<NetworkBehaviourSyncVar>
         {
             public uint netId;
-            // limtied to 255 behaviours per identity
+            // limited to 255 behaviours per identity
             public byte componentIndex;
 
             public NetworkBehaviourSyncVar(uint netId, int componentIndex) : this()
@@ -641,7 +641,7 @@ namespace Mirror
         {
             return false;
 
-            // SyncVar are writen here in subclass
+            // SyncVar are written here in subclass
 
             // if initialState
             //   write all SyncVars
@@ -737,12 +737,6 @@ namespace Mirror
         }
 
         /// <summary>
-        /// This is invoked on clients when the server has caused this object to be destroyed.
-        /// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
-        /// </summary>
-        public virtual void OnStopClient() { }
-
-        /// <summary>
         /// This is invoked for NetworkBehaviour objects when they become active on the server.
         /// <para>This could be triggered by NetworkServer.Listen() for objects in the scene, or by NetworkServer.Spawn() for objects that are dynamically created.</para>
         /// <para>This will be called for objects on a "host" as well as for object on a dedicated server.</para>
@@ -751,7 +745,7 @@ namespace Mirror
 
         /// <summary>
         /// Invoked on the server when the object is unspawned
-        /// <para>Useful for saving object data in persistant storage</para>
+        /// <para>Useful for saving object data in persistent storage</para>
         /// </summary>
         public virtual void OnStopServer() { }
 
@@ -760,6 +754,12 @@ namespace Mirror
         /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
         /// </summary>
         public virtual void OnStartClient() { }
+
+        /// <summary>
+        /// This is invoked on clients when the server has caused this object to be destroyed.
+        /// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
+        /// </summary>
+        public virtual void OnStopClient() { }
 
         /// <summary>
         /// Called when the local player object has been set up.
