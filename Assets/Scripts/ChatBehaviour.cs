ff6bff4d5baaad8022589ede39ab605cb8875f27
Tommi-Roy Kralsson <tommiroy@student.chalmers.se>
2021-02-19 13:19:35 +0100

    Started having vision

--- Assets/Scripts/ChatBehaviour.cs
+++ Assets/Scripts/ChatBehaviour.cs
@@ -1,18 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Mirror;
+using System;
+using TMPro;
 using UnityEngine;
 
-public class ChatBehaviour : MonoBehaviour
+namespace DapperDino.Mirror.Tutorials.Chat
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ChatBehaviour : NetworkBehaviour
     {
-        
-    }
+        [SerializeField] private GameObject chatUI = null;
+        [SerializeField] private TMP_Text chatText = null;
+        [SerializeField] private TMP_InputField inputField = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private static event Action<string> OnMessage;
+
+        public override void OnStartAuthority()
+        {
+            chatUI.SetActive(true);
+
+            OnMessage += HandleNewMessage;
+        }
+
+        [ClientCallback]
+        private void OnDestroy()
+        {
+            if (!hasAuthority) { return; }
+
+            OnMessage -= HandleNewMessage;
+        }
+
+        private void HandleNewMessage(string message)
+        {
+            chatText.text += message;
+        }
+
+        [Client]
+        public void Send(string message)
+        {
+            if (!Input.GetKeyDown(KeyCode.Return)) { return; }
+
+            if (string.IsNullOrWhiteSpace(message)) { return; }
+
+            CmdSendMessage(message);
+
+            inputField.text = string.Empty;
+        }
+
+        [Command]
+        private void CmdSendMessage(string message)
+        {
+            RpcHandleMessage($"[{connectionToClient.connectionId}]: {message}");
+        }
+
+        [ClientRpc]
+        private void RpcHandleMessage(string message)
+        {
+            OnMessage?.Invoke($"\n{message}");
+        }
     }
 }
