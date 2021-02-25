607abd1b105ef8dcdec0f76a0b054abf1c63fc02
Mirai ibrahim <gusibrmi@student.gu.se>
2021-02-25 21:36:11 +0100

    movement
    testing movement

---
+++ Assets/Scripts/ChildColliders.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildColliders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gameObject.tag == "Chair")
+        {
+            var child = gameObject.GetComponentInChildren<BoxCollider>();
+            child.gameObject.AddComponent<BoxCollider>();
+
+        }
+  
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
