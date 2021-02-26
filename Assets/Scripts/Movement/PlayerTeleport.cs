841c008ade49cd71e0d40729d6f31f11d5a780fc
Joakim Burman <burmanj@student.chalmers.see>
2021-02-26 09:14:28 +0100

    First draft of teleportation to projector room

---
+++ Assets/Scripts/Movement/PlayerTeleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+    {
+    public Transform teleportTarget;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start(){
+       // GetComponent(MeshRenderer).enabled = false;    
+    }
+
+    // Update is called once per frame
+    void Update() {
+       // if (distance < range) { //Checking if player is near enough
+         //   GetComponent(MeshRenderer).enabled = true;
+        }
+     //   else {
+       // GetComponent(MeshRenderer).enabled = false;
+        //}
+  //  }
+    void OnMouseDown(){
+            // this object was clicked - do something
+        player.transform.position = teleportTarget.transform.position;
+    }   
+    }
+
