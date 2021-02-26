264e9d9ef23d54d473da598c08300b35ee4db88a
Jonathan Eksberg <eksberg@student.chalmers.se>
2021-02-26 12:49:41 +0100

    First attempt on applying teleporting functionality.

---
+++ Assets/Scripts/Movement/Trigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Trigger : MonoBehaviour
+{
+    public GameObject door;
+    public void OnTriggerEnter(){
+         door.GetComponent<MeshRenderer>().enabled = true;
+     }
+     public void OnTriggerExit(){
+         door.GetComponent<MeshRenderer>().enabled = false;
+     }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject player;
+
+    public void OnMouseDown() {
+        if(GetComponent<MeshRenderer>().enabled)
+        player.transform.position = teleportTarget.transform.position;
+    }
+}
