8264c8f9436313a6382ecfdb1255b390febd6fdc
Joakim Burman <burmanj@student.chalmers.see>
2021-03-04 12:49:22 +0100

    small fixes, waiting on player movement fix

---
+++ Assets/Scripts/Movement/GroundTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GroundTrigger: MonoBehaviour
+{
+    public GameObject groundTrigger;
+    public GameObject door;
+
+    public void OnTriggerEnter(){
+         door.GetComponent<MeshRenderer>().enabled = true;
+     }
+     public void OnTriggerExit(){
+         door.GetComponent<MeshRenderer>().enabled = false;
+     }
+}
---
+++ Assets/Scripts/Movement/Teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject door;
+    public GameObject teleportLocation;
+
+
+
+    public void OnMouseDown() {
+
+        if(door.GetComponent<MeshRenderer>().enabled)
+        Debug.Log("open door");
+        player.transform.position = teleportLocation.transform.position;
+    }
+}
