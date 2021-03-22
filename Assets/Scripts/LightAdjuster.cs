acef58557991cceccdaeb2b32d613a32ba5e87e5
Tommi-Roy karlsson <tommiroy@student.chalmers.se>
2021-03-22 20:42:58 +0100

    Tidy up

--- Assets/Scripts/LightAdjuster.cs
+++ Assets/Scripts/LightAdjuster.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightAdjuster : MonoBehaviour {
-       public float maxIntensity = 10.0f;
+       public float maxIntensity = 3.0f;
        Light lightToDim;
-       float startTime;
        public bool downDimmer = false;
        public bool upDimmer = true;
        void Start() {
         lightToDim = GetComponent<Light>();
-        startTime = Time.time;
         lightToDim.intensity = maxIntensity;
        }
        public float intestitySpeed = 0.5f; //30 seconds
