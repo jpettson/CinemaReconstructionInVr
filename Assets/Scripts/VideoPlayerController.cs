8db3a13d4a6e7ad08cb828d7097486dd205c4bab
Tommi-Roy karlsson <tommiroy@student.chalmers.se>
2021-04-08 14:44:50 +0200

    Added Audio Source on canvas

--- Assets/Scripts/VideoPlayerController.cs
+++ Assets/Scripts/VideoPlayerController.cs
@@ -7,12 +7,17 @@ public class VideoPlayerController : MonoBehaviour
 {
 
     private VideoPlayer videoPlayer;
-    
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = gameObject.GetComponent<AudioSource>();
         videoPlayer = GetComponent<VideoPlayer>();
         videoPlayer.targetMaterialRenderer = GetComponentInParent<MeshRenderer>();
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
     }
 
 }
