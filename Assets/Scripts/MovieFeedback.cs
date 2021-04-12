c008637cc473ce01825a46764ecbedaf0fa71fad
Mirai ibrahim <gusibrmi@student.gu.se>
2021-04-12 20:47:44 +0200

    Additional changes
    add changes to the script

--- Assets/Scripts/MovieFeedback.cs
+++ Assets/Scripts/MovieFeedback.cs
@@ -13,42 +13,71 @@ public class MovieFeedback : MonoBehaviour
     public GameObject feedbackMenu;
     private Image image;
     public Text text;
-
-    public VideoPlayer videoPlayer;
-  
-   
-    private long currentFrame;
+    private VideoPlayer videoPlayer;
     private TextMeshProUGUI title;
     private string filePath;
+    //public Animator anim;
+
+    private bool inside = false;
 
 
 
     public void setText(string textfield)
     {
-        text.text = textfield; 
+        text.text = textfield;
     }
     void Start()
     {
-       videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer = GetComponent<VideoPlayer>();
         //   videoPlayer.targetMaterialRenderer = GetComponentInParent<MeshRenderer>();
         videoPlayer = GameObject.FindGameObjectWithTag("VideoPlayer").GetComponent<VideoPlayer>();
 
-      
+        // image = GetComponent<Image>();
+        //text = GetComponentInChildren<Text>();
+        // text.GetComponentInChildren<Text>().text = title + "is playing!";
+
+    }
+
+    void Update()
+    {
+
+        //videoPlayer = GameObject.FindGameObjectWithTag("VideoPlayer").GetComponent<VideoPlayer>();
+        /* if (inside) {
+            Debug.Log("Videoplayer is playing?");
+        } */
+        if (inside && videoPlayer.isPlaying)
+        {
+            Debug.Log("VIDEO IS PLAYING");
+            //anim.SetTrigger("Action");
+            feedbackMenu.SetActive(true);
+            string path = videoPlayer.url;
+            string name = Path.GetFileName(path).ToString();
+            Debug.Log("TITLE: " + name);
+            text.GetComponent<Text>().text = name + " is playing!";
+            //text.text = name + "is playing!";
+        }
+        else
+        {
+            feedbackMenu.SetActive(false);
+        }
 
     }
 
 
-    public void Initiate(string filePath)
+    /*  public void Initiate(string filePath)
     {
         this.filePath = filePath;
         Debug.Log(filePath);
         title = GetComponent<TextMeshProUGUI>();
         title.text = Path.GetFileNameWithoutExtension(filePath);
-    }
+    }  */
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
+        inside = true;
+
+        /* Debug.Log("ONTRIGGERENTER");
         videoPlayer = GameObject.FindGameObjectWithTag("VideoPlayer").GetComponent<VideoPlayer>();
 
         if (videoPlayer.isPlaying)
@@ -57,26 +86,29 @@ public class MovieFeedback : MonoBehaviour
             //anim.SetTrigger("Action");
             feedbackMenu.SetActive(true);
             string path = videoPlayer.url;
-            string name = Path.GetFileName(path);
+            string name = Path.GetFileName(path).ToString();
             Debug.Log("TITLE: " + name);
-            text.GetComponent<Text>().text = name + "is playing!";
-            text.text = name + "is playing!";
-        }
+            text.GetComponent<Text>().text = name + " is playing!";
+            //text.text = name + "is playing!";
+        } */
     }
 
     // Update is called once per frame
     private void OnTriggerExit(Collider other)
     {
-        if (!videoPlayer.isPlaying)
+        inside = false;
+        /* if (!videoPlayer.isPlaying)
         {
             feedbackMenu.SetActive(false);
             other.gameObject.GetComponentInChildren<SteamVR_LaserPointer>().enabled = false;
-        }
+        } */
     }
 
 
-    void Update() {
-        text.GetComponent<Text>().text = title + "is playing!";
-        text.text = title + "is playing!";
-    }
+    /*  void Update() {
+
+         text.GetComponent<Text>().text = title + "is playing!";
+         text.text = title + "is playing!";
+
+     } */
 }
