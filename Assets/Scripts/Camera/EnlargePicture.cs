f4bb14c548492423086ff497d134b872453facbd
Adam Rohdell <rohdell@student.chalmers.se>
2021-05-10 15:43:42 +0200

    Fixed picture enlargement

--- Assets/Scripts/Camera/EnlargePicture.cs
+++ Assets/Scripts/Camera/EnlargePicture.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 
 public class EnlargePicture : MonoBehaviour
 {
-    public int interpolationFramesCount = 90; // Number of frames to completely interpolate between the 2 positions
+    public int interpolationFramesCount = 120; // Number of frames to completely interpolate between the 2 positions
     int elapsedFrames = 0;
     private bool isTargeted = false;
-    private Vector3 startPosition;
-    private Vector3 endPosition;
-    private Vector3 startSize;
-    private Vector3 endSize;
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public Vector3 startSize;
+    public Vector3 endSize;
 
-
-    private void Start()
-    {
-        startPosition = transform.localPosition;
-        Debug.Log(startPosition);
-        endPosition = Vector3.Scale(transform.localPosition, new Vector3(1.08893709328f, 1, 1.054f));
-        startSize = transform.localScale;
-        endSize = Vector3.Scale(transform.localScale, new Vector3(1.1f, 1.1f, 1.1f));
-
-
-        //Enlarge();
-    }
 
 
     void Update()
@@ -31,6 +19,7 @@ public class EnlargePicture : MonoBehaviour
 
         if (isTargeted)
         {
+            Debug.Log("targeted");
             float interpolationRatio = (float)elapsedFrames / interpolationFramesCount;
 
             if (interpolationRatio < 0.99f)
